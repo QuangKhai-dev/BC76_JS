2b466e68c78c07331ee509b8c2311905ff620282
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2024-07-22 13:25:25 +0700

    update buoi 1 bien toan tu basic js

---
+++ index.js
@@ -0,0 +1,72 @@
+console.log("Tôi đang ở file index.js");
+// Biến (variable)
+// Khởi tạo biến: let + tenBien = giá trị lưu trữ
+// console.log(content);
+let content = "Anh Long say ba";
+// gán giá trị mới cho biến
+content = "Anh 5 say hello";
+console.log(content);
+console.log(content);
+console.log(content);
+console.log(content);
+console.log(content);
+console.log(content);
+console.log(content);
+console.log(content);
+
+// Hằng số (constant)
+const SO_PI = 3.15;
+// SO_PI = 3.15;
+console.log(Math.PI);
+
+// Kiểu dữ liệu
+// Kiểu dữ liệu số (number)
+let diemToan = 8;
+let diemVan = 3;
+// Kiểu dữ liệu chuỗi (string)
+let thongBao = "Hôm nay trời mưa";
+// let diemSinh = "9";
+// Kiểu dữ liệu boolean
+let emDung = true;
+let troiMua = true;
+let ngayMaiChuNhat = false;
+
+// Toán tử (Operator)
+let soDiem = 89;
+let tong = 8 + "9"; // ==> "89" ==> "300000" + 300000 ==> "300000300000"
+// với từ khoá typeof giúp kiểm tra kiểu dữ liệu
+console.log(typeof true);
+let phepChia = 30 / 3 + 10; // ? 20
+console.log(phepChia);
+/*
+aa
+aa
+vácasc
+*/
+
+// Phép tăng (++)
+let diemToanTan = 7;
+// diemToanTan++;
+// ++diemToanTan;
+
+let tinhTong = 5 + diemToanTan++ + 7 + diemToanTan; // 27
+console.log(diemToanTan);
+console.log(tinhTong);
+
+let namSinh = 2000;
+// Phép gán (+=)
+
+let bienA = 8;
+bienA = bienA + 5; // 8 + 5 ==> 13
+bienA += 5; // bienA = bienA + 5
+console.log(bienA); // 18
+let bienB = 5;
+bienB %= 2; // bienB % 2
+console.log(bienB);
+bienB *= 5;
+console.log(bienB);
+
+let giaTienMonAn = 50000;
+let tienGiamGia = giaTienMonAn * (1 - 30 / 100); //Số tiền phải trả 0.3 Tiền khuyến mãi
+let huhu;
+console.log(huhu);
